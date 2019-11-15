362cc4ca8d3e6198f31ccd2a95e08f9f62719f4c
JSONKONZEPT <conzettjason@gmail.com>
2019-11-15 16:05:30 +0100

    this is a test

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-//this is a test
+var submitGetB = document.getElementById("submitGetB"),
+    getB_aX = document.getElementById("getB_aX"),
+    getB_aY = document.getElementById("getB_aY");
+
+if(submitGetB) {
+    submitGetB.addEventListener("click",  console.log(getB(getB_aX.textContent,getB_aY.textContent,2,2)));
+} else {
+    console.log("ass")
+}
+function getB(aX, aY, abX, abY) {
+    return "" + (aX + abX) + ", " + (aY + abY);
+}
